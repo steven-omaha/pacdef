e496e394f22447fc8d4b3605207f870eb919c551
steven-omaha <35634100+steven-omaha@users.noreply.github.com>
2021-08-24 12:42:57 +0200

    fix: test get_user_confirmation

--- test_pacdef.py
+++ test_pacdef.py
@@ -107,13 +107,13 @@ class TestConfig:
         assert conf_file.is_file()
 
 
-@pytest.mark.parametrize("user_input", ["Y", "y"])
+@pytest.mark.parametrize("user_input", ["Y", "y", ""])
 def test_get_user_confirmation_continue(user_input):
     with mock.patch.object(builtins, "input", lambda _: user_input):
         assert pacdef._get_user_confirmation() is None
 
 
-@pytest.mark.parametrize("user_input", ["", "n", "N", 'asd#!|^l;"f'])
+@pytest.mark.parametrize("user_input", ["n", "N", 'asd#!|^l;"f'])
 def test_get_user_confirmation_exit(user_input):
     with mock.patch.object(builtins, "input", lambda _: user_input):
         with pytest.raises(SystemExit):
