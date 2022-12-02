c90096b9b6dfbadd31b5a7abf7a1d19a12fb29be
steven-omaha <35634100+steven-omaha@users.noreply.github.com>
2022-12-02 16:45:38 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use anyhow::Result;
 
-use pacdef::args;
-use pacdef::Group;
+use pacdef::{args, Group, Pacdef};
 
 fn main() -> Result<()> {
     let args = args::get_matched_arguments();
     let groups = Group::load_from_dir();
-    let pacdef = pacdef::Pacdef::new(args, groups);
+    let pacdef = Pacdef::new(args, groups);
     pacdef.run_action_from_arg();
     Ok(())
 }
