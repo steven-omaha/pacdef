34d7b5d9deb93ee1c40f080166ce674c19560a2f
steven-omaha <35634100+steven-omaha@users.noreply.github.com>
2023-05-15 15:58:57 +0200

    fix: build when git not installed
    When the git binary is not available, emit an empty git_hash
    so that the build continues instead of aborting due to an
    unset environment variable

--- crates/pacdef_core/build.rs
+++ crates/pacdef_core/build.rs
@@ -1,10 +1,13 @@
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("git")
+    let git_hash = match Command::new("git")
         .args(["rev-parse", "--short", "HEAD"])
         .output()
-        .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
+    {
+        Ok(output) => String::from_utf8(output.stdout).expect("git output is utf-8"),
+        _ => String::new(),
+    };
+
     println!("cargo:rustc-env=GIT_HASH={git_hash}");
 }
