a017a4ee755b8144748231fb9d43f9ab98b58b8e
teackot <teackot@gmail.com>
2023-05-16 19:07:39 +0300

    Implement flatpak_systemwide option

--- crates/pacdef_core/src/backend/actual/flatpak.rs
+++ crates/pacdef_core/src/backend/actual/flatpak.rs
@@ -27,12 +27,20 @@ const SUPPORTS_AS_DEPENDENCY: bool = false;
 impl Backend for Flatpak {
     impl_backend_constants!();
 
+    fn get_switches_runtime(&self) -> Switches {
+        if self.systemwide {
+            &[]
+        } else {
+            &["--user"]
+        }
+    }
+
     fn get_all_installed_packages(&self) -> Result<HashSet<Package>> {
-        Flatpak::get_installed_packages(true)
+        self.get_installed_packages(true)
     }
 
     fn get_explicitly_installed_packages(&self) -> Result<HashSet<Package>> {
-        Flatpak::get_installed_packages(false)
+        self.get_installed_packages(false)
     }
 
     fn make_dependency(&self, _: &[Package]) -> Result<ExitStatus> {
@@ -48,11 +56,14 @@ impl Flatpak {
         }
     }
 
-    fn get_installed_packages(include_implicit: bool) -> Result<HashSet<Package>> {
+    fn get_installed_packages(&self, include_implicit: bool) -> Result<HashSet<Package>> {
         let mut cmd = Command::new(BINARY);
         cmd.args(&["list", "--columns=application"]);
         if !include_implicit {
             cmd.arg("--app");
+        }
+        if !self.systemwide {
+            cmd.arg("--user");
         }
 
         let output = String::from_utf8(cmd.output()?.stdout)?;
